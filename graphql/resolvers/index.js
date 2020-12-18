21d01fe7fddfd4ebbaa159f2a36da0c33519e0b8
Nathaniel Wallace <nathaniel.wallace@gmail.com>
2020-12-18 17:53:12 -0500

    comments. likes, subscriptions added
    all working on the back end!

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,24 @@
-const postsResolvers = require('./posts')
-const usersResovlers = require('./users')
+const postsResolvers = require('./posts');
+const usersResovlers = require('./users');
+const commentsResovlers = require('./comments');
 
 module.exports = {
+  Post: {
+    likeCount(parent) {
+      console.log(parent);
+      return parent.likes.length;
+    },
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
-    ...postsResolvers.Query
+    ...postsResolvers.Query,
   },
   Mutation: {
-    ...usersResovlers.Mutation
-  }
-}
+    ...usersResovlers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResovlers.Mutation,
+  },
+  Subscription: {
+    ...postsResolvers.Subscription,
+  },
+};
