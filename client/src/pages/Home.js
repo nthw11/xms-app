a181ae696ca404fb56faa1f88ac9bbed5d48abed
Nathaniel Wallace <nathaniel.wallace@gmail.com>
2020-12-22 11:48:55 -0500

    live posting enabled
    posts can be written, shows up live on site

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+
 import { Grid } from 'semantic-ui-react';
 
+import { AuthContext } from '../context/auth';
+
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
+  const { user } = useContext(AuthContext);
   const { loading, data: { getPosts: posts } = {} } = useQuery(
     FETCH_POSTS_QUERY
   );
+  // const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
   return (
     <Grid columns={3}>
       <Grid.Row className='page-title'>
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
@@ -29,27 +40,5 @@ function Home() {
     </Grid>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
